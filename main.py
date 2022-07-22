406df1a2e95dc68d398988a043779b14145a1d3f
abhigamingyt <88582909+abhigamingyt@users.noreply.github.com>
2022-07-23 00:46:48 +0530

    botsamp

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+
+from dotenv import load_dotenv
+from naff import Intents
+from naff.ext.debug_extension import DebugExtension
+
+from core.base import CustomClient
+from core.extensions_loader import load_extensions
+from core.logging import init_logging
+
+if __name__ == "__main__":
+    # load the environmental vars from the .env file
+    load_dotenv()
+
+    # initialise logging
+    init_logging()
+
+    # create our bot instance
+    bot = CustomClient(
+        intents=Intents.DEFAULT,  # intents are what events we want to receive from discord, `DEFAULT` is usually fine
+    )
+
+    # load the debug extension if that is wanted
+    if os.getenv("LOAD_DEBUG_COMMANDS") == "true":
+        DebugExtension(bot=bot)
+
+    # load all extensions in the ./extensions folder
+    load_extensions(bot=bot)
+
+    # start the bot
+    bot.start("OTUwNDA0ODIwODk0OTA4NDM2.GFWUAN.P9g1uORNGTf-JlRL99gXd8B_b8QqymXOPUWGeQ")
